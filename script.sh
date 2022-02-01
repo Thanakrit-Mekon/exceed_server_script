05a4cd1103836fdec5cc98ed68d707e231b5c9b8
Thanakrit Mek-on <65077907+Thanakrit-Mekon@users.noreply.github.com>
2022-02-01 21:25:29 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,13 @@
+groupadd -g 30000 students
+mkdir /home/students
+for i in `seq -w 1 8`;
+do
+     useradd -m -d /home/students/std0$i -g 30000 -u 3000$i std0$i
+     echo -e "1q2w3e4r\n1q2w3e4r" | passwd std0$i
+     chage -d 0 std0$i
+done
+
 docker-compose -f apps/mongo/docker-compose.yml down
 rm apps/mongo/docker-compose.yml
 mv exceed_server_script/docker-compose.yml apps/mongo/docker-compose.yml
-docker-compose -d apps/mongo/docker-compose.yml up
+docker-compose -f apps/mongo/docker-compose.yml up
