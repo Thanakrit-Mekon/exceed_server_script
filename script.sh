1e3a967f9f1a160baea44ad8896258d1850b433e
Thanakrit Mek-on <65077907+Thanakrit-Mekon@users.noreply.github.com>
2022-02-01 21:38:56 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,6 +8,6 @@ do
 done
 
 docker-compose -f apps/mongo/docker-compose.yml down
-rm apps/mongo/docker-compose.yml
+cp apps/mongo/docker-compose.yml
 mv exceed_server_script/docker-compose.yml apps/mongo/docker-compose.yml
 docker-compose -f apps/mongo/docker-compose.yml up
