3bc651766c7967a9b528260bd13193b142dd0a6b
Thanakrit Mek-on <65077907+Thanakrit-Mekon@users.noreply.github.com>
2022-02-12 15:06:14 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
-groupadd -g 30000 students
-mkdir /home/students
-for i in `seq -w 1 8`;
-do
-     useradd -m -d /home/students/std0$i -g 30000 -u 3000$i std0$i
-     echo "std0$i:1q2w3e4r" | chpasswd
-     chage -d 0 std0$i
-done
+#groupadd -g 30000 students
+#mkdir /home/students
+#for i in `seq -w 1 8`;
+#do
+#     useradd -m -d /home/students/std0$i -g 30000 -u 3000$i std0$i
+#     echo "std0$i:1q2w3e4r" | chpasswd
+#     chage -d 0 std0$i
+#done
 
 docker-compose -f apps/mongo/docker-compose.yml down
 cp apps/mongo/docker-compose.yml
